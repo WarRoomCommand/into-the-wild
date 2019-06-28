928417c7656c954c6287de2aab14a9a3da936e99
WarRoomCommand <47911818+WarRoomCommand@users.noreply.github.com>
2019-06-27 21:49:23 -0400

    refined senate processing

--- congress/senate_process.py
+++ congress/senate_process.py
@@ -5,11 +5,33 @@ is_in_range = (data["chamber"] == "senate")
 spec_data = data[is_in_range]
 
 to_ising = lambda x: (2 * x) - 1.0
-output_data = spec_data.pivot_table(index="bill_id", columns="last_name", values="agree", fill_value=0)
+output_data = spec_data.pivot_table(index="bill_id", columns="last_name", values="agree", fill_value=0.5)
 output_data = output_data.transform(to_ising)
-#output_data = output_data.sort_values(by=["s36-2017"], axis=1)
+
+senators = output_data.columns
+thresh = 2 * len(senators) / 3
+for senator in senators:
+	if np.sum(abs(output_data[senator])) < thresh:
+		output_data = output_data.drop([senator], axis=1)
+		print("Removing delinquent: ", senator)
+senators = output_data.columns
+
+print()
+
+output_data = output_data.sort_values(output_data.index[9], axis=1)
 
 corr = output_data.corr()
+
+#for sen1 in senators:
+#	for sen2 in senators:
+#		if sen1 in corr.columns and sen2 in corr.columns:
+#			if sen1 != sen2 and corr[sen1][sen2] == 1:
+#				if np.sum(abs(output_data[sen1])) <= np.sum(abs(output_data[sen2])):
+#					corr = corr.drop([sen1], axis=0)
+#					corr = corr.drop([sen1], axis=1)
+#					output_data = output_data.drop([sen1], axis=1)
+#					print("Removing sheep: ", sen1)
+
 output_data_t = output_data.transpose()
 
 corr.to_csv("correlations.csv", header=False, index=False)
